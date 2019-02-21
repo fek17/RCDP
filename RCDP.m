721cc670ed6002c53e23b0fcee51a8874100b99c
Andreas Richardson <atr2717@ic.ac.uk>
2019-02-21 12:14:29 +0000

    sorting out constants

--- RCDP.m
+++ RCDP.m
@@ -1,7 +1,7 @@
 %% CE2-03-2 Group 6
 clear, clc, close all
 
-% setup constants
+% setup for constants
 global c
 
 % define constants
@@ -10,21 +10,21 @@ c.b2 = 2024;      % m^3.kmol^-1
 c.b3 = 1.0001;    % m^3.kmol^-1
 c.n = 0.428;      % dimensionless
 
-%Ln k0
+% Ln k0
 c.lnk0_1 = 19.84; 
 c.lnk0_2 = 20.34;
 c.lnk0_3 = 20.86;
 c.lnk0_4 = 18.34;
 c.lnk0_5 = 19.51;
 
-%E/R (K)
+% E/R (K)
 c.ER_1 = 6920; 
 c.ER_2 = 10436;
 c.ER_3 = 10436;
 c.ER_4 = 11457;
 c.ER_5 = 11457;
 
-%plot pressure vs riser height
+% plot pressure vs riser height
 c.rho_c=1300;              % kg.m^-3; rho_c=catalyst density
 c.eps=0.5;                 % dimensionless; eps=bed voidage
 c.g=9.81;                  % m.s^-2; g=gravitational acceleration constant
@@ -44,18 +44,18 @@ end
 
 % equations
 function dydz = odefcn(z, y)
-global c;
+global c
 
-k1 = exp((lnk0_1)-(ER_1/T));
-k2 = exp((lnk0_2)-(ER_2/T));
-k3 = exp((lnk0_3)-(ER_3/T));
-k4 = exp((lnk0_4)-(ER_4/T));
-k5 = exp((lnk0_5)-(ER_5/T));
+k1 = exp((c.lnk0_1)-(c.ER_1/T));
+k2 = exp((c.lnk0_2)-(c.ER_2/T));
+k3 = exp((c.lnk0_3)-(c.ER_3/T));
+k4 = exp((c.lnk0_4)-(c.ER_4/T));
+k5 = exp((c.lnk0_5)-(c.ER_5/T));
 dydz = zeros(6,1); % initialising array of extent of reaction
-dydz(1) = (k1*power(C_o2, n)*b1*C_ox)/(1+b1*C_ox); % dE1/dz = r1
-dydz(2) = (k2*power(C_o2, n)*b2*C_ox)/(1+b2*C_ox);
-dydz(3) = (k3*power(C_o2, n)*b2*C_ox)/(1+b2*C_ox);
-dydz(4) = (k4*power(C_o2, n)*C_pa*b3)
-dydz(5) = (k5*power(C_o2, n)*C_pa*b3)
+dydz(1) = (k1*power(C_o2, c.n)*c.b1*C_ox)/(1+c.b1*C_ox); % dE1/dz = r1
+dydz(2) = (k2*power(C_o2, c.n)*c.b2*C_ox)/(1+c.b2*C_ox);
+dydz(3) = (k3*power(C_o2, c.n)*c.b2*C_ox)/(1+c.b2*C_ox);
+dydz(4) = (k4*power(C_o2, c.n)*C_pa*c.b3);
+dydz(5) = (k5*power(C_o2, c.n)*C_pa*c.b3);
 end
 
