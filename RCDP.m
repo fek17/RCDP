a06fb2266084847ece9b7706e62265043e6021a1
Mustafa Baig <mb1217@ic.ac.uk>
2019-02-21 10:57:58 +0000

    Pressure drop v Riser height

--- RCDP.m
+++ RCDP.m
@@ -8,6 +8,16 @@ n = 0.428;      % dimensionless
 
 % equations
 
-% plot temperature vs riser height
-
 %plot pressure vs riser height
+
+rho_c=1300;              % kg.m^-3; rho_c=catalyst density
+eps=0.5;                 % dimensionless; eps=bed voidage
+g=9.81;                  % m.s^-2; g=gravitational acceleration constant
+z=0:0.1:10;              % z=riser height
+Po=1.3*1.01*10^5         %kg.m.s^-2; Po=Initial pressure
+P=Po-rho_c*(1-eps)*g*z;  %P=pressure
+plot(z,P)
+xlabel('Riser Height (m)')
+ylabel('Pressure (kg.m.s^{-2})')
+
+
