e4d9fa324ff6ff3d199177c35afab93e921b147e
Fatima <fek17@ic.ac.uk>
2019-02-21 11:13:22 +0000

    added constants

--- RCDP.m
+++ RCDP.m
@@ -5,18 +5,26 @@ b1 = 429;       % m^3.kmol^-1
 b2 = 2024;      % m^3.kmol^-1
 b3 = 1.0001;    % m^3.kmol^-1
 n = 0.428;      % dimensionless
+
+%Ln k0
 lnk0_1 = 19.84; 
 lnk0_2 = 20.34;
 lnk0_3 = 20.86;
 lnk0_4 = 18.34;
 lnk0_5 = 19.51;
-ER_1 = 
+
+%E/R (K)
+ER_1 = 6920; 
+ER_2 = 10436;
+ER_3 = 10436;
+ER_4 = 11457;
+ER_5 = 11457;
 
 %plot pressure vs riser height
 rho_c=1300;              % kg.m^-3; rho_c=catalyst density
 eps=0.5;                 % dimensionless; eps=bed voidage
 g=9.81;                  % m.s^-2; g=gravitational acceleration constant
-z=0:0.1:10;              % z=riser height
+z=0:0.1:10;              % z=riser height (m)
 Po=1.3*1.01*10^5         %kg.m.s^-2; Po=Initial pressure
 P=Po-rho_c*(1-eps)*g*z;  %P=pressure
 plot(z,P)
