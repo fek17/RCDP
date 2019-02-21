326aed9398ed4c50d770d6cfb7c36f3e25d73587
Andreas Richardson <atr2717@ic.ac.uk>
2019-02-21 09:19:00 +0000

    ooh shiny

--- RCDP.m
+++ RCDP.m
@@ -1,5 +1,7 @@
-%Constant definitions
-b1=429; %m3kmol-1
-b2=2024; %m3kmol-1
-b3=1.0001; %m3kmol-1
-n=0.428;
+%% CE2-03-2 Group 6
+
+% define constants
+b1 = 429;       % m^3.kmol^-1
+b2 = 2024;      % m^3.kmol^-1
+b3 = 1.0001;    % m^3.kmol^-1
+n = 0.428;      % dimensionless
