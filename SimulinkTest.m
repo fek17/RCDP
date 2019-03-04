1d98233d899a062efc80ef82656a072a5659f2c4
Fatima <fek17@ic.ac.uk>
2019-03-04 10:40:00 +0000

    don't screw it up

--- SimulinkTest.m
+++ SimulinkTest.m
@@ -1,5 +1,5 @@
 % Tests for Simulink Temperature Control Loop
 load_system('PA_Reactor_2019');
 set_param('PA_Reactor_2019/Cooling fluid inlet temperature (K)', 'OutValues', "[680 680 685 690 695 705 705 705 705 705].'", 'tsamp', '30');
-set_param('PA_Reactor_2019', 'Solver', 'ode15s', 'StopTime', num2str(60));
+set_param('PA_Reactor_2019', 'Solver', 'ode15s', 'StopTime', '200');
 sim('PA_Reactor_2019')
