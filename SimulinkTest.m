bb2bf6473a906fa7e89ab9a63af8f12000df14c4
Andreas Richardson <atr2717@ic.ac.uk>
2019-03-07 21:21:42 +0000

    tests & cleanup model

--- SimulinkTest.m
+++ SimulinkTest.m
@@ -1,5 +1,9 @@
-% Tests for Simulink Temperature Control Loop
-load_system('PA_Reactor_2019');
-set_param('PA_Reactor_2019/Cooling fluid inlet temperature (K)', 'OutValues', "[680 680 685 690 695 705 705 705 705 705].'", 'tsamp', '30');
-set_param('PA_Reactor_2019', 'Solver', 'ode15s', 'StopTime', '200');
-sim('PA_Reactor_2019')
+%% Tests for Simulink Temperature Control Loop
+
+%% Default test
+set_param('PA_Reactor_2019/Reactants Inlet temperature  (K)', 'OutValues', "[690 691 692 692 692 690]", 'tsamp', '10');
+set_param('PA_Reactor_2019/Cooling fluid inlet temperature (K)', 'OutValues', "[680 680 678 677 680 680]", 'tsamp', '10');
+
+%% Alessandro test
+set_param('PA_Reactor_2019/Reactants Inlet temperature  (K)', 'OutValues', "[]", 'tsamp', '10');
+set_param('PA_Reactor_2019/Cooling fluid inlet temperature (K)', 'OutValues', "[680 680 685 690 695 705 705 705 705 705]", 'tsamp', '30');
