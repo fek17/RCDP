872419ec4378800f549ab4d6afe46e0b947d69c8
Andreas Richardson <atr2717@ic.ac.uk>
2019-03-15 11:24:02 +0000

    results of final controller test

---
+++ simulation/test_plots.m
@@ -0,0 +1,90 @@
+%% CE2-03-2 Group 6
+%  plots for real-time controller test
+
+close all
+
+%% inlet temperature profiles
+
+figure
+plotStackedDataSeries(logsout,'T_r_in','T_c_in')
+
+figExport(8,6,'temp-in')
+
+%% PA flowrate
+
+figure
+plotDataSeries(logsout,'Outlet - PA flowrate');
+plotBounds([1])
+
+figExport(8,6,'PA-flow')
+
+%% cooling medium outlet
+
+figure
+plotDataSeries(logsout,'T_c_out');
+plotBounds([710 750])
+
+figExport(8,6,'temp-out')
+
+%% TC performance
+
+figure
+plotAdjacentDataSeries(logsout,'e_TC','op_TC')
+
+figExport(16,4,'TC-performance')
+
+%% FC performance
+
+figure
+plotAdjacentDataSeries(logsout,'e_FC','op_FC')
+
+figExport(16,4,'FC-performance')
+
+%% Smith predictor
+
+figure
+plotStackedDataSeries(logsout,'SP_out','T_c_out')
+
+figExport(8,6,'sp')
+
+%% plotter
+function plotDataSeries(logsout,name)
+series = getElementNames(logsout);
+index = dot(1:numel(series),strcmp(name,series));
+plot(logsout{index}.Values);
+title('');
+xlim([0 1010]);
+end
+
+function plotStackedDataSeries(logsout,name1,name2)
+subplot(2,1,1)
+plotDataSeries(logsout,name1)
+plainTextY;
+xlabel('')
+subplot(2,1,2)
+plotDataSeries(logsout,name2)
+plainTextY;
+end
+
+function plotAdjacentDataSeries(logsout,name1,name2)
+yyaxis left
+plotDataSeries(logsout,name1)
+plainTextY;
+xlabel('')
+yyaxis right
+plotDataSeries(logsout,name2)
+plainTextY;
+end
+
+function plotBounds(val)
+x = [0 1010];
+for i=1:numel(val)
+    line( [x(1); x(2)], val(i)*ones(2,1), 'Color', 'r', 'LineStyle', '--')
+end
+end
+
+function plainTextY
+ax = gca;
+set(ax.YLabel,'Interpreter','none');
+end
+
